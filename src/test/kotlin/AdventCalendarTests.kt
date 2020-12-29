74b96ac291d9271bbbf90026dbe6ee6edeb01d8a
Li Loh <lloh@squareup.com>
2020-12-30 03:05:40 +1100

    Refactor day1 input reading

--- src/test/kotlin/AdventCalendarTests.kt
+++ src/test/kotlin/AdventCalendarTests.kt
@@ -7,27 +7,31 @@ class AdventCalendarTests {
 
   @Test
   fun day1Part1Test() {
-    val expenseReport = mutableListOf<Int>()
+    val expenseReport = readDay1Input()
 
-    File("src/test/input/Day1.txt").useLines {
-      lines -> expenseReport.addAll(
-        lines.map { it.toInt() })
-    }
+    val result = adventCalendar.day1Part1(expenseReport)
 
     // 366 + 1654 = 2020, hence 366 * 1654 = 605364
-    assertEquals(605_364, adventCalendar.day1Part1(expenseReport))
+    assertEquals(605_364, result)
   }
 
   @Test
   fun day1Part2Test() {
+    val expenseReport = readDay1Input()
+
+    val result = adventCalendar.day1Part2(expenseReport)
+
+    // 1220 + 634 + 166 = 2020, hence 1220 * 634 * 166 = 128,397,680
+    assertEquals(128_397_680, result)
+  }
+
+  private fun readDay1Input(): List<Int> {
     val expenseReport = mutableListOf<Int>()
 
     File("src/test/input/Day1.txt").useLines {
       lines -> expenseReport.addAll(
       lines.map { it.toInt() })
     }
-
-    // 1220 + 634 + 166 = 2020, hence 1220 * 634 * 166 = 128,397,680
-    assertEquals(128_397_680, adventCalendar.day1Part2(expenseReport))
+    return expenseReport
   }
 }
